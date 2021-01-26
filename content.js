2de999a0184dfcaf2c858941bd1aafcd43bc03bb
SibianDG <sibian.degussem@student.hogent.be>
2021-01-26 10:23:07 +0100

    v1.6: few fixes Facebook

--- content.js
+++ content.js
@@ -18,7 +18,9 @@ class website {
     }
 
     checkAriaLabel(el, string){
-        return el.getAttribute('aria-label').includes(string);
+        if (el.getAttribute('aria-label'))
+            return el.getAttribute('aria-label').includes(string);
+        return true;
     }
 
     addToList(what, string) {
@@ -26,6 +28,7 @@ class website {
             let all = [...document.getElementsByClassName(string)];
             if (this._name === "facebook"){
                 all.forEach(classToAdd => {
+                    console.log("what")
                     if (what === "rightColumn"){
                         if (string === "rq0escxv lpgh02oy du4w35lb pad24vr5 rirtxc74 dp1hu0rb fer614ym hlyrhctz o387gat7 qbu88020 ni8dbmo4 stjgntxs czl6b2yu"){
                             if (classToAdd.getAttribute("role").includes("complementary")){
@@ -48,6 +51,12 @@ class website {
                         this._allClasses.push({what: "newsFeed", cta: classToAdd});
                     } else if (what === "overviewLeft"){
                         this._allClasses.push({what : what, cta: classToAdd});
+                    } else if(what === "rightColumn2"){
+                        console.log("rightColumn2")
+                        if (classToAdd.getAttribute("data-pagelet"))
+                            if (classToAdd.getAttribute("data-pagelet").includes("RightRail"))
+                                console.log("RightRail222222")
+                                this._allClasses.push({what : what, cta: classToAdd});
                     }
                 });
             } else if (this._name === "messenger"){
@@ -64,7 +73,6 @@ class website {
     }
 
     changeAndCheckClasses(items){
-        console.log(items)
         this._allClasses.forEach(el => {
             if (items[this._name].removeTrue.includes(el.what)){
                 if (el.cta){
@@ -76,8 +84,6 @@ class website {
                         this._needToBeHidden.push(document.getElementById("secondary-inner"));
                         this._needToBeHidden.push(document.getElementById("secondary"));
                     } else {
-                        console.log("what", el.what, "cta", el.cta);
-
                         this._needToBeHidden.push(el.cta);
                     }
                 } else {
@@ -87,6 +93,8 @@ class website {
                 this._needsToBeVisable.push(el.cta);
             }
         })
+        console.log(this._needsToBeVisable);
+        console.log(this._needToBeHidden);
         this.changeClass([...this._needToBeHidden], true);
         this.changeClass([...this._needsToBeVisable], false);
     }
@@ -156,12 +164,17 @@ function init(){
             theWebsite = new website("facebook", true)
             theWebsite.addToList("navigation","oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 j83agx80 p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl l9j0dhe7 abiwlrkh p8dawk7l bp9cbjyn cbu4d94t pi1r6xr4 taijpn5t k4urcfbm");
             theWebsite.addToList("navigation","oajrlxb2 tdjehn4e qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 j83agx80 p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl l9j0dhe7 abiwlrkh p8dawk7l bp9cbjyn s45kfl79 emlxlaya bkmhp75w spb7xbtv rt8b4zig n8ej3o3l agehan2d sk4xxmp2 taijpn5t qypqp5cg q676j6op");
+            theWebsite.addToList("navigation", "buofh1pr to382e16 o5zgeu5y jrc8bbd0 dawyy4b1 h676nmdw hw7htvoc");
             theWebsite.addToList("newsFeed","pedkr2u6 tn0ko95a pnx7fd3z");
+            theWebsite.addToList("newsFeed", "rq0escxv l9j0dhe7 du4w35lb j83agx80 g5gj957u pmt1y7k9 buofh1pr hpfvmrgz taijpn5t gs1a9yip owycx6da btwxx1t3 f7vcsfb0 fjf4s8hc b6rwyo50 oyrvap6t")
             theWebsite.addToList("rooms","gs1a9yip kb5gq1qc pfnyh3mw hpfvmrgz qdtcsgvi oi9244e8 t7l9tvuc");
             theWebsite.addToList("rightColumn","d2edcug0 hpfvmrgz qv66sw1b c1et5uql rrkovp55 a5q79mjw g1cxx5fr lrazzd5p m9osqain");
-            theWebsite.addToList("rightColumn","rq0escxv lpgh02oy du4w35lb pad24vr5 rirtxc74 dp1hu0rb fer614ym hlyrhctz o387gat7 qbu88020 ni8dbmo4 stjgntxs czl6b2yu");
+            //theWebsite.addToList("rightColumn","rq0escxv lpgh02oy du4w35lb pad24vr5 rirtxc74 dp1hu0rb fer614ym hlyrhctz o387gat7 qbu88020 ni8dbmo4 stjgntxs czl6b2yu");
+            //theWebsite.addToList("rightColumn", "rq0escxv lpgh02oy du4w35lb o387gat7 qbu88020 pad24vr5 rirtxc74 dp1hu0rb fer614ym ni8dbmo4 stjgntxs be9z9djy hlyrhctz");
+            theWebsite.addToList("rightColumn2", "cxgpxx05");
             theWebsite.addToList("overviewLeft","rq0escxv lpgh02oy du4w35lb pad24vr5 rirtxc74 dp1hu0rb fer614ym bx45vsiw o387gat7 qbu88020 ni8dbmo4 stjgntxs czl6b2yu");
             theWebsite.addToList("overviewLeft","rq0escxv lpgh02oy du4w35lb pad24vr5 rirtxc74 dp1hu0rb fer614ym rek2kq2y bx45vsiw o387gat7 qbu88020 ni8dbmo4 stjgntxs czl6b2yu");
+            theWebsite.addToList("overviewLeft","rq0escxv lpgh02oy du4w35lb o387gat7 qbu88020 pad24vr5 rirtxc74 dp1hu0rb fer614ym ni8dbmo4 stjgntxs rek2kq2y be9z9djy bx45vsiw")
         } else if (urlNow.includes("youtube.com")){
             //todo: YT don't refresh whole page when you click item...
             theWebsite = new website("youtube", false)
